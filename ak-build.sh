ca4c147eae0dce3918dcc82a0d13416db247dbd1
anarkia1976 <stefano.villa1976@gmail.com>
2013-04-24 07:37:58 +0200

    Kernel Version: AK.212.SKL.XTEST

--- ak-build.sh
+++ ak-build.sh
@@ -46,7 +46,7 @@ else
   #
   # LOCAL KERNEL VERSION
   #
-  ak_ver="AK.208.SKL.XTEST"; export LOCALVERSION="~"`echo $ak_ver`
+  ak_ver="AK.212.SKL.XTEST"; export LOCALVERSION="~"`echo $ak_ver`
 
   debug=0
 
